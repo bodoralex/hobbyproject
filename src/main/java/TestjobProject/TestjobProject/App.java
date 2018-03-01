0a789936552bd0586083bdd3eb1b4b1868c33c29
Sziszka0909 <szaboszilveszter0909@gmail.com>
2018-03-01 09:47:55 +0100

    pom.xml and src added

---
+++ src/main/java/TestjobProject/TestjobProject/App.java
@@ -0,0 +1,13 @@
+package TestjobProject.TestjobProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
